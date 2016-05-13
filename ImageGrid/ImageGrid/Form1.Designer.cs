db5c439260e8471af327275da25d2633892e1953
KorzhOnRails <korzhaleksand@gmail.com>
2016-05-13 19:03:10 +0300

    First OOP commit

---
+++ ImageGrid/ImageGrid/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace ImageGrid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxFactorIntensity1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox4, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox3, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 31);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 46.54088F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 53.45912F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(934, 795);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(470, 372);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(461, 363);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(461, 363);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(470, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(461, 363);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(3, 372);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(461, 363);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Location = new System.Drawing.Point(15, 5);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(55, 20);
+            this.openFileButton.TabIndex = 2;
+            this.openFileButton.Text = "Open";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Path";
+            // 
+            // textBoxFactorIntensity1
+            // 
+            this.textBoxFactorIntensity1.Location = new System.Drawing.Point(825, 6);
+            this.textBoxFactorIntensity1.Name = "textBoxFactorIntensity1";
+            this.textBoxFactorIntensity1.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFactorIntensity1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(683, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Enter intensity factor 1..255 :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(937, 761);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxFactorIntensity1);
+            this.Controls.Add(this.openFileButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxFactorIntensity1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ImageGrid/ImageGrid/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageGrid
+{
+
+    public partial class Form1 : Form
+    {
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+             
+        
+        }
+
+     
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+  
+
+
+
+           
+      }
+
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+
+            // Create an OpenFileDialog object.
+            OpenFileDialog openFile1 = new OpenFileDialog();
+
+            // Initialize the OpenFileDialog to look for picture.
+            openFile1.InitialDirectory = "c:\\";
+            openFile1.Filter = "Image Files(*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF|All files (*.*)|*.*";
+            openFile1.FilterIndex = 2;
+            openFile1.RestoreDirectory = true;
+
+            // Check if the user selected a file from the OpenFileDialog.
+            if (openFile1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                int width = 300, height = 300;
+                Bitmap histOriginal = new Bitmap(width, height);
+                Bitmap histResult = new Bitmap(width, height);
+                Bitmap originalImage = new Bitmap(openFile1.FileName);
+                label1.Text = openFile1.FileName;
+                pictureBox1.Image = originalImage;
+                Bitmap resultImage = originalImage;
+
+                Histogram hist = new Histogram(originalImage);
+
+                histOriginal = hist.GetHistogram();
+                byte factorIntensityValue = 100;
+                Histogram result;
+                if (textBoxFactorIntensity1.Text != "" && Byte.TryParse(textBoxFactorIntensity1.Text, out factorIntensityValue))
+                {
+                     result = new Histogram(hist.GetNormalizedImage(originalImage, (byte)factorIntensityValue));
+                     pictureBox3.Image = hist.GetNormalizedImage(originalImage, (byte)factorIntensityValue);
+                }
+                else
+                {
+                     result = new Histogram(hist.GetNormalizedImage(originalImage));
+                     pictureBox3.Image = hist.GetNormalizedImage(originalImage);
+                }
+                histResult = result.GetHistogram();
+                pictureBox1.Image = originalImage;
+                pictureBox2.Image = histOriginal;
+                
+                pictureBox4.Image = histResult;
+            }
+        }
+    }
+
+    public class Histogram
+    {
+        public int HistWidth { get; set; }
+        public int HistHeight { get; set; }
+        public Bitmap Image { get; set; }
+
+        private Bitmap Hist { get; set; }
+
+    
+        public Histogram(Bitmap image)
+        {
+            HistHeight = image.Height;
+            HistWidth = image.Width;
+            this.Image = image;
+            this.Hist = new Bitmap(image, 256, 256);
+
+           
+        }
+
+        public Bitmap GetNormalizedImage(Bitmap img , byte intensityFactor = 100)
+        {
+            Bitmap normImg = new Bitmap(img, img.Height, img.Width);
+
+            byte[] pixelIntensityArray = new byte[256];
+
+            //calculating number of pixels with intensity value of i( from 0 to 255) 
+            //and recording this number to i-element of pixelIntensityArray 
+
+            for (int i = 0; i < img.Height - 1; i++)
+            {
+                for (int j = 0; j < img.Width - 1; j++)
+                {
+                    Color pixel = img.GetPixel(j, i);
+                    pixelIntensityArray[pixel.R]++;
+                }
+            }
+
+            double[] normalizedPixelIntensityArray = new double[256];
+            double sum = 0;
+            for (int i = 0; i < pixelIntensityArray.Length; i++)
+            {
+                if (i == 0)
+                {
+                    sum = 0;
+                }
+                else
+                {
+                    sum = normalizedPixelIntensityArray[i - 1];
+                }
+                if(intensityFactor > 0 )
+                normalizedPixelIntensityArray[i] = intensityFactor*255 * pixelIntensityArray[i] / (double)(img.Width * img.Height);
+                normalizedPixelIntensityArray[i] += sum;
+                
+            }
+
+            for (int i = 0; i < normImg.Height - 1; i++)
+            {
+                for (int j = 0; j < normImg.Width - 1; j++)
+                {
+                    Color tempPix = normImg.GetPixel(j, i);
+                    Byte pixIntensity = tempPix.R;
+
+                    byte r = (byte)normalizedPixelIntensityArray[pixIntensity];
+                    byte g = r;
+                    byte b = r;
+
+                    
+                    normImg.SetPixel(j, i, Color.FromArgb(255, r, g, b));
+                }
+            }
+
+            return normImg;
+        }
+        
+        public Bitmap GetHistogram()
+        {
+                 Byte[] pixelIntensityArray = new Byte[256];
+
+                 //calculating number of pixels with intensity value of i( from 0 to 255) 
+                 //and recording this number to i-element of pixelIntensityArray 
+
+                 for (int i = 0; i < Image.Height - 1; i++)
+                 {
+                     for (int j = 0; j < Image.Width - 1; j++)
+                     {
+                         Color pixel = Image.GetPixel(j, i);
+                         pixelIntensityArray[pixel.R]++;
+                     }
+                 }
+                
+            //making histogram picture from  Bitmap image
+            for (int i = 0; i < 255; i++ )
+            {
+                for (int j = 0; j < 255; j++ )
+                {
+                    if (j < 255-pixelIntensityArray[i] )
+                    {
+                        Hist.SetPixel(i, j, Color.White);
+                    }
+                    else
+                    {
+                        Hist.SetPixel(i, j, Color.Black);
+                    }
+                }
+            }
+            return Hist;
+        }
+    }
+}
